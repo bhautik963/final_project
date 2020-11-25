e484d5d38ad7b7fe440a22e3c1be46106c00632b
bhautik patel <55433482+bhautik963@users.noreply.github.com>
2020-11-25 23:07:37 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,7 +19,7 @@ export class AppComponent implements OnInit, DoCheck {
         private _router: Router,
         private _userService: UserService
     ) {
-        this.title = 'SOCIAL DAIRY';
+        this.title = 'SOCIAL';
         this.url = GLOBAL.url;
     }
 
